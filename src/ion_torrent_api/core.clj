095fdd370a938253a2370793e7a821f8a3adef36
Michael Pheasant <mpheasant@gmail.com>
2013-10-07 08:24:53 +1000

    query experiment and results

--- src/ion_torrent_api/core.clj
+++ src/ion_torrent_api/core.clj
@@ -1,6 +1,24 @@
-(ns ion-torrent-api.core)
+(ns ion-torrent-api.core
+  (:require [clj-http.client :as client]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+
+(defn- ensure-starts-with
+  "Ensure s starts with prefix."
+  [prefix s]
+  (if (.startsWith s prefix) s (str prefix s)))
+
+(defn resource
+  [resource host creds & [opts]]
+  (:body (client/get (str "http://" host (ensure-starts-with "/rundb/api/v1/" resource))
+                     {:as :json :basic-auth creds :query-params opts})))
+
+(defn experiment
+  "Experiments that have run."
+  [host creds & [opts]]
+  (resource "experiment/" host creds (assoc opts :status__exact "run")))
+
+(defn results
+  "Results that have completed."
+  [host creds & [opts]]
+  (resource "results/" host creds (assoc opts :status__startswith "Completed")))
+
