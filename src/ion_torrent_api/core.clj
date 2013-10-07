bdf3fdba46942d3d6a99a4cca90e3bdadcec1991
Michael Pheasant <mpheasant@gmail.com>
2013-10-07 14:09:31 +1000

    helper for non-json resource

--- src/ion_torrent_api/core.clj
+++ src/ion_torrent_api/core.clj
@@ -7,7 +7,14 @@
   [prefix s]
   (if (.startsWith s prefix) s (str prefix s)))
 
+(defn resource-file
+  "Return a file from host."
+  [file-path host creds]
+  (:body (client/get (str "http://" host file-path)
+                     {:basic-auth creds})))
+
 (defn resource
+  "Return a JSON resource from host."
   [resource host creds & [opts]]
   (:body (client/get (str "http://" host (ensure-starts-with "/rundb/api/v1/" resource))
                      {:as :json :basic-auth creds :query-params opts})))
