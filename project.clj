651a1b2bbfc4e849b966d14ddf0c75aa5d63295f
Michael Pheasant <mpheasant@gmail.com>
2013-10-21 17:04:54 +1000

    push the first version

--- project.clj
+++ project.clj
@@ -1,7 +1,9 @@
-(defproject org.clojars.intronic/ion-torrent-api "0.1.0-SNAPSHOT"
+(defproject intronic/ion-torrent-api "0.1.0"
   :description "Ion Torrent Server API"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/intronic/ion-torrent-api"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [clj-http "0.7.7"]])
+                 [clj-http "0.7.7"]]
+  :scm {:name "git"
+        :url "https://github.com/intronic/ion-torrent-api"})
