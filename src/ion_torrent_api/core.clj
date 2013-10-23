e861b4cea444f53e95ba7af82ce505184a7af23f
Michael Pheasant <mpheasant@gmail.com>
2013-10-23 14:12:18 +1000

    refactor to not coerce json keys as keywords since they can contain spaces, and generate invalid keywords (that cant be written/read)

--- src/ion_torrent_api/core.clj
+++ src/ion_torrent_api/core.clj
@@ -5,8 +5,8 @@
 (defn- pluginresult-api-path
   "API path to pluginresult files."
   [res]
-  (let [{path :path
-         report-link :reportLink} res]
+  (let [{path "path"
+         report-link "reportLink"} res]
     ;; sample path:
     ;;   "/results/analysis/output/Home/Auto_user_AIB-24-AmpliSeq_CCP_24_50_061/plugin_out/coverageAnalysis_out"
     ;; sample reportLink:
@@ -37,56 +37,57 @@
     dest-file))
 
 (defn resource
-  "Return a JSON resource from host."
+  "Return a JSON resource from host.
+Keys are not coerced to keywords as the JSON keys can have spaces in them which are invalid as keywords and not printable+readable."
   [host creds resource & [opts]]
   (:body (client/get (str "http://" host (ensure-starts-with "/rundb/api/v1/" resource))
-                     {:as :json :basic-auth creds :query-params opts})))
+                     {:as :json-string-keys :basic-auth creds :query-params opts})))
 
 (defn experiment
   "Experiments that have run."
   [host creds & [opts]]
-  (resource "experiment/" host creds (assoc opts :status__exact "run")))
+  (resource "experiment/" host creds (assoc opts "status__exact" "run")))
 
 (defn experiment-name
   "Experiment by name."
   [host creds name & [opts]]
-  (let [{{tot :total_count} :meta
-         exp :objects} (resource host creds "experiment/" (merge opts {:expName__exact name :status__exact "run"}))]
+  (let [{{tot "total_count"} "meta"
+         exp "objects"} (resource host creds "experiment/" (merge opts {"expName__exact" name "status__exact" "run"}))]
     (if (= 1 tot) (first exp))))
 
 (defn results
   "Results that have completed."
   [host creds & [opts]]
-  (resource host creds "results/" (assoc opts :status__startswith "Completed")))
+  (resource host creds "results/" (assoc opts "status__startswith" "Completed")))
 
 (defn experiment-results
   "Results that have completed for an experiment and are not thumbnails."
   [host creds exp]
-  (remove (comp :thumb :metaData)
-          (map #(resource host creds % {:status__startswith "Completed"}) (:results exp))))
+  (remove #(get-in % ["metaData" "thumb"])
+          (map #(resource host creds % {"status__startswith" "Completed"}) (get exp "results"))))
 
 (defn experiment-pluginresults
   "Plugin results that have completed for an experiment."
   [host creds exp]
-  (map #(resource host creds % {:status__exact "Completed"})
-       (mapcat :pluginresults (experiment-results host creds exp))))
+  (map #(resource host creds % {"status__exact" "Completed"})
+       (mapcat #(get % "pluginresults") (experiment-results host creds exp))))
 
 (defn experiment-coverage
   "coverageAnalysis plugin results that have completed, for an experiment."
   [host creds exp]
-  (filter #(-> % :plugin :name (= "coverageAnalysis"))
+  (filter #(-> % (get-in ["plugin" "name"]) (= "coverageAnalysis"))
           (experiment-pluginresults host creds exp)))
 
 (defn experiment-variants
   "variantCaller plugin results that have completed, for an experiment."
   [host creds exp]
-  (filter #(-> % :plugin :name (= "variantCaller"))
+  (filter #(-> % (get-in ["plugin" "name"]) (= "variantCaller"))
           (experiment-pluginresults host creds exp)))
 
 (defn pluginresult
   "Pluginresult that have completed."
   [host creds & [opts]]
-  (resource host creds "pluginresult/"  (assoc opts :status__startswith "Completed")))
+  (resource host creds "pluginresult/"  (assoc opts "status__startswith" "Completed")))
 
 (defn pluginresult-id
   "Pluginresult that have completed."
@@ -96,24 +97,24 @@
 (defn coverage
   "coverageAnalysis for id."
   [host creds id]
-  (let [{{name :name} :plugin :as res} (pluginresult-id host creds id)]
+  (let [{{name "name"} "plugin" :as res} (pluginresult-id host creds id)]
     (if (= "coverageAnalysis" name) res)))
 
 (defn variant-call
   "variantCall for id."
   [host creds id]
-  (let [{{name :name} :plugin :as res} (pluginresult-id host creds id)]
+  (let [{{name "name"} "plugin" :as res} (pluginresult-id host creds id)]
     (if (= "variantCaller" name) res)))
 
 (defn coverage-amplicon-file-path
-  "Coverage by amplicon file path."
+  "Coverage by amplicon file path. Barcode is a keyword or string."
   [cov barcode]
-  (let [{{{{prefix :Alignments} (keyword barcode)} :barcodes} :store} cov
+  (let [{{{{prefix "Alignments"} (name barcode)} "barcodes"} "store"} cov
         path (str (pluginresult-api-path cov) "/" (name barcode) "/" prefix ".amplicon.cov.xls")]
     path))
 
 (defn tsvc-variant-file-path
-  "TSVC variant vcf file path."
+  "TSVC variant vcf.gz file path. Barcode is a keyword or string."
   [res barcode]
   (let [path (str (pluginresult-api-path res) "/" (name barcode) "/TSVC_variants.vcf.gz" )]
     path))
@@ -122,5 +123,5 @@
 
 #_(defn coverage-amplicon-files
   [cov]
-  (map #(coverage-amplicon-file-path cov (name %)) (-> cov :store :barcodes keys)))
+  (map #(coverage-amplicon-file-path cov (name %)) (-> cov (get-in ["store" "barcodes"]) keys)))
 
