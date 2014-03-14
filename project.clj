b8ef1dbe816d646337976f604647408f9fbffad7
Michael Pheasant <mpheasant@gmail.com>
2014-03-14 18:48:57 +1000

    upgrade package

--- project.clj
+++ project.clj
@@ -20,7 +20,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/algo.generic "0.1.1"]
+                 [org.clojure/algo.generic "0.1.2"]
+                 ;;[clj-http "0.9.0"]
                  [clj-http "0.7.9"]]
 
   :source-paths ["src"]
