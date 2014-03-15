ad35a87d684f253592dad5dc94bcb99fe4513dcd
Michael Pheasant <mpheasant@gmail.com>
2014-03-15 15:37:15 +1000

    get file as json

--- test/ion_torrent_api/test/core.clj
+++ test/ion_torrent_api/test/core.clj
@@ -8,12 +8,14 @@
 (def creds ["dummy" "dummy"])
 (def host "http://my-intranet-torrent-server.com")
 
-(defn uri-to-edn-file
-  [uri]
-  (str "test/data" (str/replace uri #"/$" "") ".edn"))
+(defn uri-to-file
+  [uri & [ext]]
+  (str "test/data" (str/replace uri #"/$" "") (if ext (str "." (name ext)))))
 
-(expect "test/data/my/path/to/file.edn" (uri-to-edn-file "/my/path/to/file/"))
-(expect "test/data/my/path/to/file.edn" (uri-to-edn-file "/my/path/to/file"))
+(expect "test/data/my/path/to/file.edn" (uri-to-file "/my/path/to/file/" :edn))
+(expect "test/data/my/path/to/file.edn" (uri-to-file "/my/path/to/file" :edn))
+(expect "test/data/my/path/to/file.json" (uri-to-file "/my/path/to/file/" :json))
+(expect "test/data/my/path/to/file" (uri-to-file "/my/path/to/file/"))
 
 ;;; test private functions by accessing value of symbols directly
 (expect "abcdef" (#'ion/ensure-starts-with "abc" "def"))
@@ -27,7 +29,7 @@
 (expect {:status 200}
         (in (with-fake-routes-in-isolation
               {#".*/rundb/api/v1/.*" (fn [{uri :uri :as req}]
-                                       {:status 200 :headers {} :body (pr-str (slurp (uri-to-edn-file uri)))})}
+                                       {:status 200 :headers {} :body (slurp (uri-to-file uri :json))})}
 
               (client/get (str host  "/rundb/api/v1/" "experiment/schema/")))))
 
