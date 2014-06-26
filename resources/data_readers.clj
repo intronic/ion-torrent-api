2f44f28080e75b9c9a4c376971ba0d3611516355
Michael Pheasant <mpheasant@gmail.com>
2014-06-26 13:22:05 +1000

    move everything into one ns so implementations and protocol and records defined together

--- resources/data_readers.clj
+++ resources/data_readers.clj
@@ -1,9 +1,9 @@
-{ion_torrent_api.schema.TorrentServer ion-torrent-api.schema/map->TorrentServer
- ion_torrent_api.schema.Experiment    ion-torrent-api.schema/map->Experiment
- ion_torrent_api.schema.Result        ion-torrent-api.schema/map->Result
- ion_torrent_api.schema.PluginResult  ion-torrent-api.schema/map->PluginResult
- ;; for any data saved before refactoring
- ion_torrent_api.core.TorrentServer ion-torrent-api.schema/map->TorrentServer
- ion_torrent_api.core.Experiment    ion-torrent-api.schema/map->Experiment
- ion_torrent_api.core.Result        ion-torrent-api.schema/map->Result
- ion_torrent_api.core.PluginResult  ion-torrent-api.schema/map->PluginResult}
+{ion_torrent_api.core.TorrentServer ion-torrent-api.core/map->TorrentServer
+ ion_torrent_api.core.Experiment    ion-torrent-api.core/map->Experiment
+ ion_torrent_api.core.Result        ion-torrent-api.core/map->Result
+ ion_torrent_api.core.PluginResult  ion-torrent-api.core/map->PluginResult
+  ;; for any data saved before second refactoring
+ ion_torrent_api.schema.TorrentServer ion-torrent-api.core/map->TorrentServer
+ ion_torrent_api.schema.Experiment    ion-torrent-api.core/map->Experiment
+ ion_torrent_api.schema.Result        ion-torrent-api.core/map->Result
+ ion_torrent_api.schema.PluginResult  ion-torrent-api.core/map->PluginResult}
--- src/ion_torrent_api/core.clj
+++ src/ion_torrent_api/core.clj
@@ -5,12 +5,168 @@
             [clojure.algo.generic.functor :refer (fmap)]
             [clojure.instant :as inst]
             [slingshot.slingshot :refer (try+ throw+)]
-            [ion-torrent-api.schema :refer :all])
-  (:import [ion_torrent_api.schema Experiment Result PluginResult TorrentServer]))
+            [schema.core :as s]
+            [schema.macros :as sm]))
 
 (declare get-json ensure-starts-with filter-latest-result plugin-result-type-map
          barcode-eas-map plugin-result-api-path-prefix
          file-name BUFFER-SIZE)
+
+(defprotocol TorrentServerAPI
+  "Torrent Server API calls."
+  (experiments [this] [this opts] [this limit offset]
+    "Get experiments (with options 'opts' or by limit and offset).")
+  (experiment-name [this name] [this name opts]
+    "Get experiment by name (with options 'opts').")
+  (experiment [this] [this id-or-uri] [this id-or-uri opts]
+    "Get experiment by id or uri (with options 'opts').")
+  (result [this] [this id-or-uri] [this id-or-uri opts]
+    "Get result by id or uri (with options 'opts').")
+  (plugin-result [this] [this id-or-uri] [this id-or-uri opts]
+    "Get plugin-result by id or uri (with options 'opts').")
+  (lib-metrics [this] [this id-or-uri] [this id-or-uri opts]
+    "Get lib-metrics by id or uri (with options 'opts').")
+  (tf-metrics [this] [this id-or-uri] [this id-or-uri opts]
+    "Get tf-metrics by id or uri (with options 'opts').")
+  (analysis-metrics [this] [this id-or-uri] [this id-or-uri opts]
+    "Get analysis-metrics by id or uri (with options 'opts').")
+  (quality-metrics [this] [this id-or-uri] [this id-or-uri opts]
+    "Get quality-metrics by id or uri (with options 'opts').")
+  (barcode-set [this] [this exp]
+    "Set of barcodes.")
+  (barcode-map [this] [this exp]
+    "Map of barcodes to values.")
+  (complete? [this])
+  (coverage? [this]
+    "Coverage analysis plugin.")
+  (coverage [this]
+    "Coverage analysis plugin.")
+  (variant-caller? [this]
+    "Variant caller plugin.")
+  (variant-caller [this]
+    "Variant caller plugin.")
+  (sample-id? [this]
+    "Sample ID plugin.")
+  (sample-id [this]
+    "Sample ID plugin.")
+  (bam-uri [this bc]
+  "BAM uri for barcode.")
+  (bai-uri [this bc]
+  "BAM BAI uri for barcode.")
+  (bam-header-uri [this bc]
+    "BAM header uri for barcode. ")
+  (pdf-uri [this]
+    "PDF report uri.")
+  (tsvc-vcf-uri [this bc]
+    "TorrentSuite VCF uri for barcode.")
+  (tsvc-vcf-tbi-uri [this bc]
+    "TorrentSuite VCF TBI uri for barcode.")
+  (tsvc-variants-xls-uri [this bc]
+    "TorrentSuite variants XLS uri for barcode.")
+  (tsvc-alleles-xls-uri [this bc]
+    "TorrentSuite alleles XLS uri for barcode.")
+  (tsvc-target-bed-uri [this]
+    "TorrentSuite target bed uri.")
+  (coverage-ampl-uri [this bc]
+    "Amplicon Coverage analysis uri for barcode."))
+
+(sm/defrecord TorrentServer
+    [server-url :- s/Str
+     version :- s/Keyword
+     api-path :- s/Str]
+  Object
+  (toString [this] (pr-str this)))
+
+(sm/defrecord PluginResult
+    [type :- (s/maybe s/Keyword)
+     torrent-server :- TorrentServer
+     id :- s/Int
+     uri :- s/Str
+     result-uri :- s/Str
+     result-name :- s/Str
+     state :- s/Str
+     path :- s/Str
+     report-link :- s/Str
+     name :- s/Str
+     version :- s/Str
+     versioned-name :- s/Str
+     library-type :- (s/maybe s/Str)
+     config-desc :- (s/maybe s/Str)
+     target-name :- (s/maybe s/Str)
+     target-bed :- (s/maybe s/Str)
+     experiment-name :- (s/maybe s/Str)
+     trimmed-reads? :- (s/maybe s/Bool)
+     barcode-result-map :- (s/maybe {s/Str s/Any})
+     barcoded? :- s/Bool
+     start-time :- s/Inst
+     end-time :- s/Inst
+     raw-map :- {s/Any s/Any}]
+  Object
+  (toString [this] (pr-str this)))
+
+(sm/defrecord Result
+    [torrent-server :- TorrentServer
+     id :- s/Int
+     name :- s/Str
+     uri :- s/Str
+     experiment-uri :- s/Str
+     status :- s/Str
+     plugin-result-uri-set :- [s/Str]
+     plugin-state-map :- {s/Str s/Str}
+     analysis-version :- s/Str
+     report-status :- s/Str
+     plugin-store-map :- {s/Str {s/Str s/Any}}
+     bam-link :- s/Str
+     fastq-link :- s/Str
+     report-link :- s/Str
+     filesystem-path :- s/Str
+     reference :- s/Str
+     lib-metrics-uri-set :- [s/Str]
+     tf-metrics-uri-set :- [s/Str]
+     analysis-metrics-uri-set :- [s/Str]
+     quality-metrics-uri-set :- [s/Str]
+     timestamp :- s/Inst
+     thumbnail? :- s/Bool
+     plugin-result-set :- (s/maybe #{PluginResult})
+     lib-metrics-set :- (s/maybe #{{s/Any s/Any}})
+     tf-metrics-set :- (s/maybe #{{s/Any s/Any}})
+     analysis-metrics-set :- (s/maybe #{{s/Any s/Any}})
+     quality-metrics-set :- (s/maybe #{{s/Any s/Any}})
+     raw-map :- {s/Any s/Any}]
+  Object
+  (toString [this] (pr-str this)))
+
+(sm/defrecord Experiment
+    [torrent-server :- TorrentServer
+     id :- s/Int
+     name :- s/Str
+     pgm-name :- s/Str
+     display-name :- s/Str
+     uri :- s/Str
+     run-type :- s/Str
+     chip-type :- s/Str
+     result-uri-set :- [s/Str]
+     dir :- s/Str
+     status :- s/Str
+     ftp-status :- s/Str
+     sample-map :- {s/Str {s/Any s/Any}}
+     barcode-sample-map :- {s/Str s/Str}
+     date :- s/Inst
+     latest-result-date :- s/Inst
+     latest-result :- (s/maybe Result)
+     raw-map :- {s/Any s/Any}]
+  Object
+  (toString [this] (pr-str this)))
+
+(def data-readers
+  {'ion_torrent_api.schema.TorrentServer ion-torrent-api.core/map->TorrentServer
+   'ion_torrent_api.schema.Experiment    ion-torrent-api.core/map->Experiment
+   'ion_torrent_api.schema.Result        ion-torrent-api.core/map->Result
+   'ion_torrent_api.schema.PluginResult  ion-torrent-api.core/map->PluginResult
+   'ion_torrent_api.core.TorrentServer   ion-torrent-api.core/map->TorrentServer
+   'ion_torrent_api.core.Experiment      ion-torrent-api.core/map->Experiment
+   'ion_torrent_api.core.Result          ion-torrent-api.core/map->Result
+   'ion_torrent_api.core.PluginResult    ion-torrent-api.core/map->PluginResult})
 
 (defn torrent-server
   [server-url & {:keys [creds version api-path] :or {version :v1}}]
@@ -20,7 +176,7 @@
 
 (extend-protocol TorrentServerAPI
 
-  ion_torrent_api.schema.Experiment
+  ion_torrent_api.core.Experiment
   (barcode-set [this] (into #{} (keys (barcode-map this))))
   (barcode-map [this] (-> this :barcode-sample-map))
   (complete? [this] (= ["run" "Complete"] (-> this ((juxt :status :ftp-status)))))
@@ -40,7 +196,7 @@
   (sample-id [this]
     (->> this :latest-result :plugin-result-set (filter sample-id?)))
 
-  ion_torrent_api.schema.Result
+  ion_torrent_api.core.Result
 
   (barcode-set     ;    "Set of all barcodes found in any plugin or experiment."
     ([this]
@@ -94,7 +250,7 @@
   (complete? [this] (= "Completed" (-> this :status)))
   (pdf-uri [this] (format "/report/latex/%d.pdf" (-> this :id)))
 
-  ion_torrent_api.schema.PluginResult
+  ion_torrent_api.core.PluginResult
 
   (barcode-set
     ([this] (into #{} (keys (barcode-map this))))
@@ -137,7 +293,7 @@
   ;; reportLink: "/output/Home/XXX-24-YYY/"
   ;; API path:   "/output/Home/XXX-24-YYY/plugin_out/coverageAnalysis_out"
 
-  ion_torrent_api.schema.TorrentServer
+  ion_torrent_api.core.TorrentServer
 
   (experiments
     ([this] (experiments this {}))
